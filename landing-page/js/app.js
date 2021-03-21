a6571596a03118fd6d6b4cb790358b6eb381ed60
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2021-03-21 12:52:46 -0700

    the y position of the webpage

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,39 +1,66 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
+
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
+// This determines the position of the screen. Edited from
+// https://www.kirupa.com/html5/get_element_position_using_javascript.htm
+// to fit the need of the program
+function scrollPosition(el) {
+  var yPos = 0;
+
+  while (el) {
+    if (el.tagName == "BODY") {
+      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+      xPos += (el.offsetLeft - xScroll + el.clientLeft);
+      yPos += (el.offsetTop - yScroll + el.clientTop);
+    } else {
+      // for all other non-BODY elements
+      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+    }
+
+    el = el.offsetParent;
+  }
+  return {
+    x: xPos,
+    y: yPos
+  };
+}
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
+
 
 
 // Add class 'active' to section when near top of viewport
@@ -45,13 +72,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
