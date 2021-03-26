0143ab6fa2b17859f507840cab8557b2205a5edf
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2021-03-25 19:42:27 -0700

    too many errors, redoing code

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -17,11 +17,6 @@
  * Define Global Variables
  *
 */
-var nav = document.getElementById('navbar__list');
-var sectionList = document.getElementsByClassName('landing__container');
-var activeClass = "your-active-class";
-let activeSection = document.getElementById('section1');
-
 
 /**
  * End Global Variables
@@ -31,38 +26,9 @@ let activeSection = document.getElementById('section1');
 
 // build the nav
 
-for (let i = 1; i <= sectionList.length; i++) {
-  var box = document.createElement('li');
-  box.setAttribute('id', '#nav' + i);
-  let navId = document.getElementById('#nav' + i);
-  var uList = document.createElement('a');
-  uList.classList.add('menu__link');
-  uList.setAttribute('href', '#section' + i);
-  uList.innerText = 'Section' + i;
-  box.appendChild(uList);
-  nav.appendChild(box);
-};
 
 // Add class 'active' to section when near top of viewport
-let navActive = document.getElementById("nav1")
 
-for (let i = 1; i <= sectionList.length; i++) {
-  var navId = document.getElementById('nav' + i);
-  let sections = document.getElementById('section' + i);
-    navId.addEventListener('click', function(e) {
-      e.preventDefault();
-      sections.scrollIntoView();
-
-      navActive.classList.remove(activeClass);
-      navId.classLisst.add(activeClass);
-      navActive = navId;
-
-      var section = document.getElementById('section' + i);
-      activeSection.classList.remove(activeClass);
-      section.classList.add(activeClass);
-      activeSection = section;
-    });
-};
 
 
 // Scroll to anchor ID using scrollTO event
