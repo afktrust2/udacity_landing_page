3dbab261cf70bc7eb227357f316ee0c0ef3aa22f
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2021-03-26 11:33:23 -0700

    Last edits to make the code more readable.

--- landing-page/README.md
+++ landing-page/README.md
@@ -11,3 +11,6 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+-added text-
+This project was to make a dynamic navigation bar that interacted with the page and highlighted the section which the reader is on. Some of the code was by a Udacity mentor with some changes to it to fit my needs of the program.
--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -11,44 +11,44 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Define Global Variables
  *
-*/
-
+ */
 let navbarList = document.querySelector("#navbar__list");
-var sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll("section");
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
-*/
+ */
 
-//Function to check if an element is in viewport or not
+//Function to check if an element is in viewport or not. Added from a Udacity
+//Mentor.
 function isInViewport(elem) {
-	var distance = elem.getBoundingClientRect();
+    const distance = elem.getBoundingClientRect();
 
-	return (
-		distance.top >= -300 &&
-		distance.left >= 0 &&
-		distance.bottom <= (1.3 * window.innerHeight) &&
-		distance.right <= (window.innerWidth)
-	);
+    return (
+        distance.top >= -300 &&
+        distance.left >= 0 &&
+        distance.bottom <= (1.3 * window.innerHeight) &&
+        distance.right <= (window.innerWidth)
+    );
 };
 
 //Function to remove active classes
 function deactivateSections() {
-    sections.forEach((element)=>{
+    sections.forEach((element) => {
         element.classList.remove("your-active-class", "active");
     });
 }
 
 function deactivateNavLinks() {
     let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
-    navbarAnchors.forEach((element)=>{
+    navbarAnchors.forEach((element) => {
         element.classList.remove("active-nav");
     });
 }
@@ -57,7 +57,7 @@ function deactivateNavLinks() {
  * End Helper Functions
  * Begin Main Functions
  *
-*/
+ */
 
 // Add class 'active' to section when near top of viewport
 function activateCurrentSection(currentSection) {
@@ -69,26 +69,26 @@ function activateCurrentSection(currentSection) {
 
 function activateNavLinks(currentSectionId) {
     let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
-        navbarAnchors.forEach((element)=>{
-            if(element.getAttribute('href') == `#${currentSectionId}`) {
-                element.classList.add("active-nav");
-            }
-        });
+    navbarAnchors.forEach((element) => {
+        if (element.getAttribute('href') == `#${currentSectionId}`) {
+            element.classList.add("active-nav");
+        }
+    });
 }
 
 /**
  * End Main Functions
  * Begin Events
  *
-*/
+ */
 
 // Build menu
 function buildNavbar() {
-	sections.forEach((element)=>{
-	    let listItem = document.createElement("li");
-	    listItem.classList.add("navbar__list__item");
-    	let sectionName = element.getAttribute("data-nav");
-    	let currentId = element.getAttribute("id");
+    sections.forEach((element) => {
+        let listItem = document.createElement("li");
+        listItem.classList.add("navbar__list__item");
+        let sectionName = element.getAttribute("data-nav");
+        let currentId = element.getAttribute("id");
         listItem.innerHTML = `<a href="#${currentId}" class="nav__hyperlink">${sectionName}</a>`;
         navbarList.appendChild(listItem);
     });
@@ -98,16 +98,16 @@ function buildNavbar() {
 buildNavbar();
 
 // Set sections as active
-window.addEventListener('scroll', function (event) {
-	event.preventDefault();
+window.addEventListener('scroll', function(event) {
+    event.preventDefault();
 
     sections.forEach((element) => {
         if (isInViewport(element)) {
             deactivateSections();
             activateCurrentSection(element);
-        } else if(window.scrollY==0) {
+        } else if (window.scrollY == 0) {
             deactivateSections();
             deactivateNavLinks();
         }
-    }, );
+    });
 });
